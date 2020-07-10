3f88aec45448310a46c7c9e7eebaed73c9b20cac
Joseph <wisdombegins78@gmail.com>
2020-07-10 13:00:25 -0400

    centered the elements & set transition.

--- resources/views/dashboardagent.blade.php
+++ resources/views/dashboardagent.blade.php
@@ -26,69 +26,99 @@
     align-self: center;
 }
 .btn-primary {
+
     background-color: #0089BA!important;
-    color: white;
+    color: #fff!important;
     font-size: 1.8em;
     transition: .5s ease;
-    width: ;
 }
 .btn-primary:hover {
     background-color: white!important;
-    color: #0089BA!important; 
+    color: #0089BA!important;
 }
-.cover {   
-    text-shadow: 0 0 5px #555;
+
+.cover { 
+    visibility: hidden;
     position: absolute;
-    width: 100%;
-    top: 50%;
-    height: 130px; 
+    opacity: 0;
+    text-shadow: 0 0 5px #555;
+    width: 500px;
     padding: 50px;
     text-align: center;
-    font-size: 1em;
-   background-color: rgba(88,88,88,0.9);
+    font-size: 1.3em;
+    top: -30px;
+    left: -125px;
+   background-color: rgba(9, 87, 170, 0.85);
+   -webkit-transition: opacity 1800ms, visibility 1800ms;
+   transition: opacity 1800ms, visibility 1800ms;
    color: #fff;
    border: 2px solid white;
    box-shadow: 5px 5px 15px #000;
    border-radius: 8px;
+   z-index: 9999;
 }
-#cover {
-    display: none;
-}
-
-
-
 </style>
+
+                
 <div class="content">
     <div class="container-fluid">
         <div class="grid-container">
-            <div class="dash-content text-center" style="position: relative;">
+            <div class="dash-content text-center" >
                 <div class="dash-logo height: 200px;">
                     <img src="/light-bootstrap/img/logo.jpg" class="img-thumbnail my-4"> 
                 </div>
-                <ul class=" text-center">
-                    <!-- This should show how many leads that are in the database that hasn't been sent to an agents email yet. -->
-                    <li>Available Leads: <span class="amount" id="availableLeads">300</span></li>
-                    <!-- This should show how many that the logged in agent have generated. -->
-                    <li>Leads Sent: <span class="amount" id="leadsGenerated">5</span></li>
-                </ul>
-                <div id="cover" class="cover" title="A new lead has been sent to your inbox.">A lead has been sent to your email.</div>
-                  <a href="#" id="generateLeadBtn" class="btn btn-primary" onclick="lead()" title="Click here to send a lead to your inbox.">Send a Lead</a>
+
+                <div class="container"> 
+                    <div class="row justify-content-center">
+                        <!-- This should show how many leads that are in the database that hasn't been sent to an agents email yet. -->
+                        <div class="card bg-light mb-3" style="max-width: 18rem;">
+                    
+                          <div class="card-body">
+                            <h5 class="card-title">Available Leads:</h5>
+                            <p class="card-text"><span class="amount" id="availableLeads">300</span></p>
+                          </div>
+                        </div>
+                      
+                        <!-- This should show how many that the logged in agent have generated. -->
+                         <div class="card bg-light mb-3" style="max-width: 18rem;">
+                       
+                          <div class="card-body">
+                            <h5 class="card-title">Leads Sent:</h5>
+                            <p class="card-text"><span class="amount" id="availableLeads">5</span></p>
+                          </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                    <div style="position: relative">
+                        <div  class="cover" title="A new lead has been sent to your inbox.">
+                         A lead has been sent to your email.</div>
+                        <a href="#" id="generateLeadBtn" class="btn btn-primary" onclick="lead()" title="Click here to send a lead to your inbox.">
+                        Send a Lead
+                        </a>
+                        <!-- Box that covers the button on click -->     
+                    </div> 
+                </div>
             </div>
         </div>
     </div>
 </div>
 <script type="text/javascript">
     const leadBtn = document.querySelector("#generateLeadBtn") ;
-    const cover = document.querySelector("#cover")
+    const cover = document.querySelector(".cover")
 
 
 function lead() {
-    cover.style.display = "block"
-    leadBtn.style.color = "#eee!important"
-    setTimeout(() => cover.style.cursor = "not-allowed", 700);
-    setTimeout(function() {
-    cover.style.display = "none";
-    leadBtn.style.color = "#fff!important"
+    cover.style.visibility = "visible";
+    cover.style.opacity = 1;
+    leadBtn.classList.add('disabled');
+    leadBtn.style.color = "#fff!important";
+    cover.style.cursor = "not-allowed";
+    setTimeout(() => {
+    cover.style.visibility = "hidden";
+    cover.style.opacity = 0;
+    leadBtn.classList.remove('disabled'); 
+    location.reload();
     }, 10000);
 }
     
