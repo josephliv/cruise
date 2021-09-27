8e311b56670b6b79f04bf7d475350bf0b67cf856
gsantoswh <dyegofern@gmail.com>
2021-09-26 21:11:27 -0400

    test

--- app/Mail/LeadSent.php
+++ app/Mail/LeadSent.php
@@ -33,6 +33,7 @@ class LeadSent extends Mailable
     {
         $mailable = $this
                     ->subject($this->lead->subject . ' -||' . $this->lead->id)
+                    ->replyTo('sales@cruisertravels.com')
                     ->view('mails.leadsent');
 
         Log::debug($this->lead->attachment);
