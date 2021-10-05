acc2f0b770ee0196c2d1d6a70670abd3b8456140
gsantoswh <dyegofern@gmail.com>
2021-10-05 13:44:59 -0400

    test

--- resources/views/mails/leadsent.blade.php
+++ resources/views/mails/leadsent.blade.php
@@ -1,4 +1,5 @@
 Original Date:{{\Carbon\Carbon::parse($lead->received_date)->format('m/d/Y g:i A')}}<br/>
 Original Sender:{{$lead->email_from}}<br/>
+Original Attachment:<a href="https://leads.cruisertravels.com/storage/{{$lead->attachment}}">Attachment</a><br/>
 <hr/>
 {!!$lead->body!!}
