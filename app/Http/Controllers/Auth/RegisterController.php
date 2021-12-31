cbefac47b49361cca9622d605245379bd9b1b0fb
Tim Brownlaw <tim.brownlaw@galaticbioware.com>
2021-12-31 12:11:58 +1100

    Logging New user registrations to file
    Using Log, info is saved in log/laravel.log for now.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -64,6 +65,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        Log::info("New User:Name:{$data['name']}- Email: {$data['email']}");
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
