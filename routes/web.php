6633d0d0bddb9317c6ed8bd1101fe31592b49e0c
Tim Brownlaw <tim.brownlaw@galaticbioware.com>
2022-01-05 18:17:16 +1100

    Fixed missing route for user/store
    This had been removed using D's updated files

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,5 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('reademail', 'MailBoxController@index');
 	Route::get('leads/get', 'MailBoxController@sendLeads');
 	Route::get('{page}', ['as' => 'page.index', 'uses' => 'PageController@index']);
+    Route::post('user/store', ['as' =>'user.store', 'uses' => 'UserController@store']);
 });
