8314a88fea60eb1728eb045ed56c4653d719ea89
“Joseph <wisdombegins78@gmail.com>
2022-01-21 22:27:19 -0500

    removed time at the bottom of the sidebar.

--- resources/views/layouts/navbars/sidebaragent.blade.php
+++ resources/views/layouts/navbars/sidebaragent.blade.php
@@ -57,11 +57,7 @@ Tip 2: you can also add an image using data-image tag
                     </ul>
                 </div>
             </li>
-            <li style="position: relative; top: 450px; padding: 20px;">
-                 <?php
-                        echo "The time is " . date("h:i a");
-                    ?>
-            </li>
+            
         </ul>
     </div>
 </div>
