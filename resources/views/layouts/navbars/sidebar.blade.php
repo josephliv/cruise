5390a9512b274304d95f207c1fe0e9c3f233bcc6
“Joseph <wisdombegins78@gmail.com>
2022-01-22 17:19:52 -0500

    Added a new sidebar to work on all screen sizes and backedup the orginal files.

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,63 +1,51 @@
-<div class="sidebar" data-color="blue" data-image="{{ asset('light-bootstrap/img/sidebar-4.jpg') }}">
-    <!--
-Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
-
-Tip 2: you can also add an image using data-image tag
--->
-
-    <div class="sidebar-wrapper">
-        <div class="logo">
-            <a href="/" class="simple-text">
-                <img style="border-radius: 8px;"  src="/light-bootstrap/img/logo.jpg">
-            </a>
-        </div>
-        <ul class="nav">
-
-            <div id="mobileSideMenu" class="d-none"></div>
-
-            <li class="nav-item @if($activePage == 'dashboard') active @endif">
-                <a class="nav-link" href="{{route('dashboard')}}">
-                    <i class="nc-icon nc-chart-pie-35"></i>
-                    <p>{{ __("Dashboard") }}</p>
-                </a>
-            </li>
-
-            <li class="nav-item">
-
-                <div class="collapse @if($activeButton =='laravel') show @endif" id="laravelExamples">
-                    <ul class="nav">
-                        <li class="nav-item @if($activePage == 'user') active @endif">
-                            <a class="nav-link" href="{{route('profile.edit')}}">
-                                <i class="nc-icon nc-single-02"></i>
-                                <p>{{ __("My Profile") }}</p>
-                            </a>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="{{route('dashboard')}}">
+    <img style="border-radius: 4px; " src="/light-bootstrap/img/logo.jpg">
+  </a>
+  <span class="logged-in-as"><small>Logged in as {{ \Auth::user()->name }}</small></span>
+  <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="nc-icon nc-sun-fog-29"></i>&nbsp;{{ __('Log out') }} </a>
+  <hr style="border-color: #fff; width: 80%; margin: 20px auto;">
+  <a href="{{route('dashboard')}}" title="Return to the dashboard"><i class="nc-icon nc-chart-pie-35"></i>&nbsp;{{ __("Dashboard") }}</a>
+  <a href="{{route('profile.edit')}}" title="View or edit your profile"><i class="nc-icon nc-single-02"></i>&nbsp;{{ __("My Profile") }}</a>
+  <a href="{{route('user.index')}}" title="View or edit your agents"><i class="nc-icon nc-circle-09"></i>&nbsp;{{ __("Manage Agents") }}</a>
+  <a href="{{route('emails.manage')}}" title="View or edit the leads"><i class="nc-icon nc-email-85"></i>&nbsp;{{ __("Manage Leads") }}</a>
+  <a href="{{route('priorities.index')}}" title="View or edit the priorities"><i class="nc-icon nc-preferences-circle-rotate"></i>&nbsp;{{ __("Rules & Priorities") }}</a>
+  
+  <a title="View a list of links" data-toggle="collapse" href="#usefulLinks" @if($activeButton=='laravel') aria-expanded="true" @endif><i class="nc-icon nc-tap-01"></i>&nbsp;{{ __('Useful Links') }}</a>
+  
+<div class="collapse @if($activeButton =='laravel') hidden @endif" id="usefulLinks">
+    <ul class="nav usefulLinks">
+        <li class="nav-item @if($activePage == 'user') active @endif">
+            <a class="nav-link" target="_blank" rel="noreferral" href="https://fs8.formsite.com/loundo1/s5qym0uua9/index.html">REPORT A NEW BOOKING</a>
+        </li>
+        <li>
+                            <a class="nav-link" target="_blank" rel="noreferral" href="http://www.cruisertravels.com/ta-training.html">TRAINING VIDEOS</a>
                         </li>
-                        <li class="nav-item @if($activePage == 'user-management') active @endif">
-                            <a class="nav-link" href="{{route('user.index')}}">
-                                <i class="nc-icon nc-circle-09"></i>
-                                <p>{{ __("Manage Agents") }}</p>
-                            </a>
+                        <li>
+                            <a class="nav-link" target="_blank" rel="noreferral" rel="noreferral" href="https://WWW.GOCCL.COM">CARNIVAL</a>
                         </li>
-
-                        <li class="nav-item @if($activePage == 'leads-management') active @endif">
-                            <a class="nav-link" href="{{route('emails.manage')}}">
-                                <i class="nc-icon nc-email-85"></i>
-                                <p>{{ __("Manage Leads") }}</p>
-                            </a>
-                        </li>
-                        <li class="nav-item @if($activePage == 'priority-management') active @endif">
-                            <a class="nav-link" href="{{route('priorities.index')}}">
-                                <i class="nc-icon nc-preferences-circle-rotate"></i>
-                                <p>{{ __("Rules & Priorities") }}</p>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            
+                        <li><a class="nav-link" target="_blank" rel="noreferral" href="https://WWW.CRUISINGPOWER.COM">ROYAL/CELEBRITY/AZAMARA</a></li>
+                        <li><a class="nav-link" target="_blank" rel="noreferral" href="https://WWW.FIRSTMATES.COM">VIRGIN VOYAGES</a></li>
+                        <li><a class="nav-link" target="_blank" rel="noreferral" href="https://accounts.havail.sabre.com/login/cruises/home?goto=https://cruises.sabre.com/SCDO/login.jsp">SABRE GDS </a></li>
+                        <li><a class="nav-link" target="_blank" rel="noreferral" href=" https://www.vaxvacationaccess.com">VAX LAND BDS </a></li>
+                        <li><a class="nav-link" target="_blank" rel="noreferral" href="http://rccl.force.com/directtransfers/DTTRoyal">ROYAL TRANSFER LINK</a></li>
+                        <li><a class="nav-link" target="_blank" rel="noreferral" href="http://rccl.force.com/directtransfers/DTTCelebrity">CELEBRITY TRANSFER LINK</a></li>
+                        <li><a class="nav-link" target="_blank" rel="noreferral" href="http://www.americanexpress.com/asdonline">AMEX PLATINUM PERKS</a></li>
+                        <li><a class="nav-link" target="_blank" rel="noreferral" href="www.agent.uplift.com ">UPLIFT</a></li>
         </ul>
-        
-       
-    </div>
-    
 </div>
+
+</div>
+
+<span style="margin: 0 10px;font-size:30px;cursor:pointer;color:#001f8b;" onclick="openNav()">&#9776;</span>
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
